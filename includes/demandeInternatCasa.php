7385203d568dd9242f6e551f08f154a48441977b
AdamAitOufkir <adamoufkir05@gmail.com>
2024-02-19 00:08:21 +0100

    demander internat

--- includes/demandeInternatCasa.php
+++ includes/demandeInternatCasa.php
@@ -16,7 +16,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     $output = "<div class='RoomList'>";
     $output .= "<table id='data-table'>";
-    $output .= "<thead><tr><th>Numéro de demande</th><th>Nom de l'étudiant</th><th>Sexe</th><th>Date de création</th><th>Numéro de chambre</th><th>Nombre d'étudiants dans la chambre</th><th>Nombre de demandes pour la chambre</th><th colspan=2>Action</th></tr></thead>";
+    $output .= "<thead><tr><th>Numéro de demande</th><th>Nom de l'étudiant</th><th>Sexe</th><th>Date de création</th><th colspan=2>Action</th></tr></thead>";
     $output .= "<tbody>";
 
     while ($row = $result->fetch_assoc()) {
@@ -27,9 +27,6 @@ if ($result->num_rows > 0) {
         $output .= "<td>" . $row['name'] . "</td>";
         $output .= "<td>" . ($row['genre'] == 'boy' ? 'Garçon' : 'Fille') . "</td>";
         $output .= "<td>" . $date = date('d-m-Y', strtotime($row['created_at'])) . "</td>";
-        $output .= "<td>" . $row['room_alias'] . "</td>";
-        $output .= "<td style='font-weight:bold;" . ($numStudentsInRoom == 4 ? 'color:red;' : 'color:green;') . "'>" . $numStudentsInRoom . "</td>";
-        $output .= "<td>" . $numRequestsInRoom . "</td>";
         $output .= "<td style='border-right: none;'><a class='validate' href='internat_demandes_validation.php?request_id=" . $row['id_demande'] . "&amp;name=" . urlencode($row['name']) . "'>Valider</a></td>";
         $output .= "<td style='border-left: none;'><a class='reject' href='internat_demandes_rejection.php?request_id=" . $row['id_demande'] . "&amp;name=" . urlencode($row['name']) . "'>Rejeter</a></td>";
         $output .= "</tr>";
